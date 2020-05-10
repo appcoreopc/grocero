e27a6fce2b6c10f8f4beb63daafba3373beb6b93
jeremy <jeremy@jeremys-MBP.hub>
2020-05-10 12:09:37 +0800

    Added product count info

--- lib/components/imagelistview.dart
+++ lib/components/imagelistview.dart
@@ -6,18 +6,17 @@ import 'package:grocero/style/appstyle.dart';
 
 class ImageListViewState<T extends StatefulWidget> extends State<T> {
   Future<List<ProductListingModel>> _futureDataSource;
- 
+  Map<String, int> productCount = Map<String, int>();
+
   @override
   void initState() {
-
     super.initState();
     _futureDataSource = MockDataService.GetProductListing();
   }
 
   @override
   Widget build(BuildContext context) {
-
-     _futureDataSource = MockDataService.GetProductListing();
+    _futureDataSource = MockDataService.GetProductListing();
 
     return Scaffold(
         body: FutureBuilder<List<ProductListingModel>>(
@@ -44,13 +43,14 @@ class ImageListViewState<T extends StatefulWidget> extends State<T> {
         });
   }
 
-  Widget _buildRow(ProductListingModel newsData) {
+  Widget _buildRow(ProductListingModel productListingData) {
     return Ink(
       child: ListTile(
-        title: Text(newsData.title, style: AppStyle.listViewFontStyle),
+        title: Text(productListingData.title,
+            style: AppStyle.listViewTitleFontStyle),
         subtitle: Padding(
             padding: EdgeInsets.fromLTRB(0, 12, 0, 12),
-            child: Image.network(newsData.urlToImage)),
+            child: buildChildLayout(productListingData)),
         trailing: Icon(
           Icons.arrow_forward,
           color: Colors.white,
@@ -61,5 +61,74 @@ class ImageListViewState<T extends StatefulWidget> extends State<T> {
       ),
       color: Colors.grey,
     );
+  }
+
+  Column buildChildLayout(ProductListingModel productListingData) {
+    return Column(children: <Widget>[
+      Image.network(productListingData.urlToImage),
+      Padding(padding: EdgeInsets.all(10)),
+      Text(productListingData.description,
+          style: AppStyle.listViewTitleFontStyle),
+      Padding(padding: EdgeInsets.all(6)),
+      Text(productListingData.content,
+          style: AppStyle.listViewContentFontStyle),
+      ButtonBar(
+        children: <Widget>[
+            _buildProductOrderCount(productListingData.title),
+          
+          FlatButton(
+            color: Colors.black,
+            child: const Text('Grab'),
+            onPressed: () {
+              _addProduct(productListingData.title, 1);
+            },
+          ),
+          FlatButton(
+            color: Colors.black,
+            child: const Text('Remove'),
+            onPressed: () {
+              _removeProduct(productListingData.title, 1);
+            },
+          ),
+        ],
+      )
+    ], crossAxisAlignment: CrossAxisAlignment.start);
+  }
+
+  void _addProduct(String productName, int quantity) {
+    if (productCount.keys.contains(productName)) {
+      setState(() {
+        productCount[productName] = productCount[productName] + quantity;
+      });
+    } else {
+      setState(() {
+        productCount[productName] = quantity;
+      });
+    }
+  }
+
+  void _removeProduct(String productName, int quantity) {
+    if (productCount.keys.contains(productName)) {
+      setState(() {
+        productCount[productName] = productCount[productName] - quantity;
+      });
+    }
+  }
+
+  // Widget _buildProductOrderCount(int count) {
+  //   if (count > 0) {
+  //     return Text(count.toString(), style: AppStyle.listViewTitleFontStyle);
+  //   } else {
+  //     return Text("", style: AppStyle.listViewTitleFontStyle);
+  //   }
+  // }
+
+  Widget _buildProductOrderCount(String title) {
+    if (title != null && productCount.keys.contains(title)) {
+      var count = productCount[title];
+      return Text(count.toString(), style: AppStyle.listViewTitleFontStyle);
+    } else {
+      return Text("", style: AppStyle.listViewTitleFontStyle);
+    }
   }
 }
--- lib/style/appstyle.dart
+++ lib/style/appstyle.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 
 class AppStyle {
 
- static final TextStyle listViewFontStyle =
-      TextStyle(fontSize: 15.0, color: Colors.black.withOpacity(0.8));
+ static final TextStyle listViewTitleFontStyle =
+      TextStyle(fontSize: 16.0, color: Colors.black.withOpacity(0));
+
+ static final TextStyle listViewContentFontStyle =
+      TextStyle(fontSize: 14.0, color: Colors.black.withOpacity(0.8));
 }
